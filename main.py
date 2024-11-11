6c9856e4d2af44fb7ef8988632e70e73eba8cea0
Adil Keku Gazder <69271111+ag826@users.noreply.github.com>
2024-11-11 03:50:54 +0000

    test

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-"""
-Main cli or app entry point
-"""
+from mylib/extract import extract
+from transform_load import load
 
-from mylib.calculator import add
-import click
-
-#var=1;var=2
-
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
-
+# from query import create
+# from query import read
+# from query import update
+# from query import delete
+from query import crud
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    url = "https://raw.githubusercontent.com/nogibjj/ag825_sqlite_lab/refs/heads/main/Cancer_Data.csv"
+    file_path = "Cancer_Data.csv"
+    database = "CancerDB.db"
+
+    # Extract
+    print("Extracting data...")
+    extract(url, file_path)
+
+    # Transform and load
+    print("Transforming data...")
+    load()
+
+    # Query
+    print("Querying data...")
+    crud()
+    # read(database)
+    # create(database)
+
+    # update(database)
+    # delete(database)
